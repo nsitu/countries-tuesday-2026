388555f8c662e7f55660f92cc5683fd0fb529f86
Harold Sikkema <harold@nsitu.ca>
2026-09-15 13:00:48 -0400

    syntax

--- script.js
+++ script.js
@@ -32,7 +32,7 @@ async function fetchAndDisplayCountries() {
         `<img class="flag" src="${flagImage}">
         <h4>${country?.names?.common}</h4>
         <p>${country?.area} km²</p> 
-        <p><b>Lat/Lng</b> ${country?coordinates?.lat}°,${country?.coordinates?.lng}°</p> ` 
+        <p><b>Lat/Lng</b> ${country?.coordinates?.lat}°,${country?.coordinates?.lng}°</p> ` 
       
       document.querySelector('#countries') .appendChild(div)
     })
